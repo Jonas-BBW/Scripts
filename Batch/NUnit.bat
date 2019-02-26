14fca2b943dbd1b7adc71ec0fb5f04060cb72a1b
Jonas-BBW <46409140+Jonas-BBW@users.noreply.github.com>
2019-02-26 10:28:43 +0100

    Update NUnit.bat

--- Batch/NUnit.bat
+++ Batch/NUnit.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-cls
+CLS
 ECHO Geben Sie den Namen der Projektmappe mit den Tests ein!
 ECHO Auf dem Desktop wird dann eine Datei erstellt, die die
 ECHO Befehle zum Installieren der benoetigten Pakete enthaelt.
@@ -7,13 +7,8 @@ ECHO Kopieren Sie diese Befehle in die Paket-Manager-Konsole!
 SET /p projekt=Projektname:
 
 ECHO Install-Package NUnit -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-
 ECHO Install-Package NUnit.ConsoleRunner -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-
 ECHO Install-Package NUnit.Extension.NUnitProjectLoader -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-
 ECHO Install-Package NUnit.Extension.VSProjectLoader -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-
 ECHO Install-Package NUnit3TestAdapter -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-
 ECHO Update-Package >>Z:\Desktop\NuGet.txt
