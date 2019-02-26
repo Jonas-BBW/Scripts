d2920fb770b2e36d765771b37baa83ba9a7e96c1
Jonas-BBW <46409140+Jonas-BBW@users.noreply.github.com>
2019-02-26 10:47:44 +0100

    Update NUnit.bat

--- Batch/NUnit.bat
+++ Batch/NUnit.bat
@@ -6,9 +6,9 @@ ECHO Befehle zum Installieren der benoetigten Pakete enthaelt.
 ECHO Kopieren Sie diese Befehle in die Paket-Manager-Konsole!
 SET /p projekt=Projektname:
 
-ECHO Install-Package NUnit -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-ECHO Install-Package NUnit.ConsoleRunner -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-ECHO Install-Package NUnit.Extension.NUnitProjectLoader -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-ECHO Install-Package NUnit.Extension.VSProjectLoader -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-ECHO Install-Package NUnit3TestAdapter -ProjectName %projekt% >>Z:\Desktop\NuGet.txt
-ECHO Update-Package >>Z:\Desktop\NuGet.txt
+ECHO Install-Package NUnit -ProjectName %projekt% >>%~dp0NuGet.txt
+ECHO Install-Package NUnit.ConsoleRunner -ProjectName %projekt% >>%~dp0NuGet.txt
+ECHO Install-Package NUnit.Extension.NUnitProjectLoader -ProjectName %projekt% >>%~dp0NuGet.txt
+ECHO Install-Package NUnit.Extension.VSProjectLoader -ProjectName %projekt% >>%~dp0NuGet.txt
+ECHO Install-Package NUnit3TestAdapter -ProjectName %projekt% >>%~dp0NuGet.txt
+ECHO Update-Package >>%~dp0NuGet.txt
